872c06598664dc01aa3dd67c9862f5d73a877259
DanilaEfimov <efimovdanila.de@gmail.com>
2024-09-03 16:22:31 +0400

    Update menubar.cpp

--- CLASSES/menubar.cpp
+++ CLASSES/menubar.cpp
@@ -1,44 +1,99 @@
-#ifndef MENUBAR_H
-#define MENUBAR_H
+#include "menubar.h"
 
-#include<QMenuBar>
-#include<QMenu>
-#include<QAction>
-#include<QMainWindow>
+QMenu** MenuBar::menues = nullptr;
+QAction** MenuBar::actions = nullptr;
+QMenuBar* MenuBar::bar = nullptr;
 
-class MenuBar : public QMenuBar
+MenuBar::MenuBar() {
+    this->setVisible(true);
+
+    initActions();
+    initMenues();
+    bindActions();
+    bindMenues();
+}
+
+MenuBar::~MenuBar() {
+    deleteActions();
+    deleteMenues();
+    delete bar;
+}
+
+QMenuBar *MenuBar::getMenuBar() {
+    return bar;
+}
+
+QMenu *MenuBar::getMenu(uint index)
 {
-    Q_OBJECT
-public:
-    MenuBar();
-    ~MenuBar();
+    if(index >= menuc){
+        return nullptr;
+    }
+    else{
+        return menues[index];
+    }
+}
 
-    // Accessors
-    static QMenuBar* getMenuBar();
-    static QMenu* getMenu(uint index);
+void MenuBar::initActions()
+{
+    actions = new QAction*[actc];
+    for(uint i = 0; i < actc; i++){
+        actions[i] = new QAction;
+    }
+    // set names
+    actions[0]->setText("New Session"); // for file menu
+    actions[1]->setText("Save");
+    actions[2]->setText("White theme"); // for view menu
+    actions[3]->setText("Black theme");
+    actions[4]->setText("Right mode");
+    actions[5]->setText("Left mode");
+    actions[6]->setText("Help");        // for line menu
+    actions[7]->setText("Open line");
+    actions[8]->setText("History");
+}
 
-private:
-    static QMenuBar* bar;
+void MenuBar::initMenues()
+{
+    menues = new QMenu*[menuc];
+    for(uint i = 0; i < menuc; i++){
+        menues[i] = new QMenu;
+    }
+    // set names
+    menues[0]->setTitle("File");
+    menues[1]->setTitle("View");
+    menues[2]->setTitle("Command line");
+}
 
-    static const uint menuc = 3;    // file, view, command line
-    static const uint actc = 9;
-    // file: save, new session;
-    // view: white theme, black theme, right mode, left mode;
-    // command line: help, open line, history;
+void MenuBar::bindActions()
+{
+    menues[0]->addAction(actions[0]);   // file menu
+    menues[0]->addAction(actions[1]);
+    menues[1]->addAction(actions[2]);   // view menu
+    menues[1]->addAction(actions[3]);
+    menues[1]->addAction(actions[4]);
+    menues[1]->addAction(actions[5]);
+    menues[2]->addAction(actions[6]);   // line menu
+    menues[2]->addAction(actions[7]);
+    menues[2]->addAction(actions[8]);
+}
 
-    static QMenu** menues;
-    static QAction** actions;
+void MenuBar::bindMenues()
+{
+    bar = new QMenuBar;
+    for(uint i = 0; i < menuc; i++){
+        bar->addMenu(menues[i]);
+    }
+}
 
-    // Construct
-    static void initActions();
-    static void initMenues();
-    static void bindActions();
-    static void bindMenues();
+void MenuBar::deleteActions() {
+    for(uint i = 0; i < actc; i++){
+        delete actions[i];
+    }
+    delete[] actions;
+}
 
-    // Destruct
-    static void deleteActions();
-    static void deleteMenues();
-
-};
-
-#endif // MENUBAR_H
+void MenuBar::deleteMenues() {
+    for(uint i = 0; i < menuc; i++){
+        delete menues[i];
+    }
+    delete[] menues;
+}
